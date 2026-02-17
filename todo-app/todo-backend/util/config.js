93b19d66f39bc7b4f104a8fbcaf295cad1351029
Kai Vorma <kai.vorma@iki.fi>
2026-02-17 21:33:44 +0200

    major rework on compose files

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://the_username:the_password@localhost:3456/the_database'
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://the_username:the_password@localhost:27017/the_database'
 const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 module.exports = {
